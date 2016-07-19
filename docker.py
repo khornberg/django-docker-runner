b981dc5058a97e0e136ad71193785ac956214b5f
Kyle Hornberg <khornberg@users.noreply.github.com>
2016-07-19 11:04:32 -0400

    Working example

--- docker.py
+++ docker.py
@@ -1,23 +1,16 @@
-from django_nose.runner import NoseTestSuiteRunner
 from django.db import connections
-from django.conf import settings
-
+try:
+    from django_nose.runner import NoseTestSuiteRunner
+except ImportError:
+    pass
 import docker
-
-
-class DockerRunner(object):
-    # maybe subclass in an init after determining if nose or stock django should be used
-    def __init__(self, *args, **kwargs):
-        if 'django_nose' in settings.INSTALLED_APPS:
-            DockerNoseRunner(args, kwargs)
-        return None
 
 
 class DockerNoseRunner(NoseTestSuiteRunner):
 
     def __init__(self, *args, **kwargs):
         try:
-            self.client = docker.Client(base_url='unix://var/tmp/docker.sock')
+            self.client = docker.Client(base_url='unix://var/run/docker.sock')
         except Exception as e:
             print('init error', e)
         return super(NoseTestSuiteRunner, self).__init__(args, kwargs)
@@ -26,38 +19,15 @@ class DockerNoseRunner(NoseTestSuiteRunner):
         for a in connections:
             c = connections[a]
 
-            name = c.settings_dict.get('NAME', 'name')
-            password = c.settings_dict.get('PASSWORD', '')
-            user = c.settings_dict.get('USER', name)
-            port = c.settings_dict.get('PORT', 5432)
+            engine = c.settings_dict.get('ENGINE', '')
+            if 'postgres' in engine:
+                name, password, user, port, host = self.setup_postgres(c)
 
             c.settings_dict['PORT'] = port
             c.settings_dict['USER'] = user
             c.settings_dict['PASSWORD'] = password
             c.settings_dict['NAME'] = name
-
-            try:
-                self.container = self.client.create_container(
-                    image='postgres:9.4',
-                    name=name,
-                    environment={
-                        'POSTGRES_PASSWORD': password,
-                        'POSTGRES_USER': user,
-                    },
-                    host_config=self.client.create_host_config(port_bindings={port: port})
-                )
-                self.client.start(self.container)
-                print('Waiting on database to start...')
-                n = 0
-                for line in self.client.logs(container=self.container, stream=True):
-                    log = line.decode('utf-8')
-                    print(log)
-                    if 'LOG:  autovacuum launcher started' in log:
-                        if n is 1:
-                            break
-                        n += 1
-            except Exception as e:
-                print('>>>>>>>>>>>>>>>>>>', e)
+            c.settings_dict['HOST'] = host
 
             return super(NoseTestSuiteRunner, self).setup_databases()
 
@@ -66,3 +36,37 @@ class DockerNoseRunner(NoseTestSuiteRunner):
         self.client.stop(self.container)
         self.client.remove_container(self.container)
         return
+
+    def setup_postgres(self, connection):
+        name = connection.settings_dict.get('NAME', 'postgres-django-docker-runner')
+        password = connection.settings_dict.get('PASSWORD', '')
+        user = connection.settings_dict.get('USER', name)
+        port = connection.settings_dict.get('PORT', 5432)
+        port = 5432 if port == '' else port
+        host = connection.settings_dict.get('HOST', '127.0.0.1')
+        host = '127.0.0.1' if host in ['localhost', ''] else host  # do not use localhost socket
+        try:
+            self.container = self.client.create_container(
+                image='postgres:9.4',
+                name='{}_docker'.format(name),
+                environment={
+                    'POSTGRES_PASSWORD': password,
+                    'POSTGRES_USER': user,
+                },
+                host_config=self.client.create_host_config(port_bindings={port: port})
+            )
+            self.client.start(self.container)
+            print('Starting database...')
+            # Hack to know when postgress is actually ready to accept connections
+            n = 0
+            for line in self.client.logs(container=self.container, stream=True):
+                log = line.decode('utf-8')
+                # print(log)
+                if 'LOG:  autovacuum launcher started' in log:
+                    if n is 1:
+                        break
+                    n += 1
+        except Exception as e:
+            print('>>>>>>>>>>>>>>>>>>', e)
+
+        return name, password, user, port, host
